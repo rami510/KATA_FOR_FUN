62162ab0d997ef965814e51fa32adcb393f66248
unknown <rami.bouabid@gmail.com>
2023-06-06 21:38:10 +0200

    succeeded test with number 5

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
@@ -9,9 +9,13 @@ class KataForFunService {
         val result = StringBuilder()
     	if(inputNumber % 3 == 0){
     		result.append("kata")
-    	}
+    	}else if(inputNumber % 5 == 0){
+			result.append("For")
+		}
     	if(inputNumber.toString().contains("3")){
     		result.append("Kata")
+    	}else if(inputNumber.toString().contains("5")){
+    		result.append("For")
     	}
 
         return result.toString().ifEmpty{
