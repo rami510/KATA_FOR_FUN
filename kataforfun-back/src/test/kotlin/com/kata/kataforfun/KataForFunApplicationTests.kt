cb424a5f29323b612bd62c087f544cf82358ec3d
unknown <rami.bouabid@gmail.com>
2023-06-06 22:31:05 +0200

    code succeeded tests

--- kataforfun-back/src/test/kotlin/com/kata/kataforfun/KataForFunApplicationTests.kt
+++ kataforfun-back/src/test/kotlin/com/kata/kataforfun/KataForFunApplicationTests.kt
@@ -32,5 +32,35 @@ class KataForFunApplicationTests {
 	@Test
 	fun testKataForFunShouldReturnFun() {
 		assertEquals("Fun",kataForFunService.convertNumber(7))
+	}	
+	
+	@Test
+	fun testKataForFunShouldReturnKata() {
+		assertEquals("Kata",kataForFunService.convertNumber(9))
+	}	
+	
+	@Test
+	fun testKataForFunShouldReturnKataFor() {
+		assertEquals("KataFor",kataForFunService.convertNumber(51))
+	}
+	
+	@Test
+	fun testKataForFunShouldReturnForKata() {
+		assertEquals("ForKata",kataForFunService.convertNumber(53))
+	}
+	
+	@Test
+	fun testKataForFunShouldReturnKataKataKata() {
+		assertEquals("KataKataKata",kataForFunService.convertNumber(33))
+	}
+	
+	@Test
+	fun testKataForFunShouldReturnKataFun() {
+		assertEquals("KataFun",kataForFunService.convertNumber(27))
+	}	
+	
+	@Test
+	fun testKataForFunShouldReturnKataForFor() {
+		assertEquals("KataForFor",kataForFunService.convertNumber(15))
 	}
 }
